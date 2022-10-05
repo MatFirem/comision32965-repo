ffbff201053292deb0d62fffdd0fc7360e4886ba
MatFirem <matiaseschwab@gmail.com>
2022-10-05 18:52:42 -0300

    V 0.0.1

---
+++ Matias_Schwab_Desafio2/Assets/Scripts/desafio2.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class desafio2 : MonoBehaviour
+{
+    public float vida = 100;
+    public float velocidad = 0.1f;
+    public Vector3 direccion;
+   
+
+    void Start()
+    {
+        Debug.Log(vida);
+       
+         
+    }
+
+    void Update()
+    {
+        transform.position += direccion * velocidad * Time.deltaTime;
+        CurarJugador();
+        HerirJugador();
+
+        Debug.Log(vida);
+             
+    }
+
+    void CurarJugador()
+    {
+        vida = vida + 1 * Time.deltaTime;
+    }
+    void HerirJugador()
+    {
+        vida = vida - 5 * Time.deltaTime;
+    }
+   
+}
